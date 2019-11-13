b2e608570c2f18316f13dfe467463bcd186f5914
Che-An <an5566x@gmail.com>
2019-11-13 20:22:47 +0800

    Updates

---
+++ precache-manifest.50cfcab9e93fc3c32a12e33e89eca1a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75087e968803bd6d1845b62614d80a97",
+    "url": "/Dasre/react-my-pages/index.html"
+  },
+  {
+    "revision": "734fc9fe35eafc83faaf",
+    "url": "/Dasre/react-my-pages/static/css/2.d5dbc5a1.chunk.css"
+  },
+  {
+    "revision": "0a7f0da8ca7c7803ac3e",
+    "url": "/Dasre/react-my-pages/static/css/main.486e6363.chunk.css"
+  },
+  {
+    "revision": "734fc9fe35eafc83faaf",
+    "url": "/Dasre/react-my-pages/static/js/2.0cb0bd4e.chunk.js"
+  },
+  {
+    "revision": "0a7f0da8ca7c7803ac3e",
+    "url": "/Dasre/react-my-pages/static/js/main.e2b23bf5.chunk.js"
+  },
+  {
+    "revision": "9c9920e39929d9ffc9ae",
+    "url": "/Dasre/react-my-pages/static/js/runtime-main.d9b30c4f.js"
+  },
+  {
+    "revision": "b3eb09945f13166b8d51714b19991db3",
+    "url": "/Dasre/react-my-pages/static/media/3-1.b3eb0994.png"
+  },
+  {
+    "revision": "630a41718a6ef4274e1b0d9c5b5a08fe",
+    "url": "/Dasre/react-my-pages/static/media/3-2.630a4171.png"
+  },
+  {
+    "revision": "d757b2ffa317bc61a81d8ecfcc480141",
+    "url": "/Dasre/react-my-pages/static/media/3-3.d757b2ff.png"
+  },
+  {
+    "revision": "6c98b6201298d37ffe68e1062e6a22c4",
+    "url": "/Dasre/react-my-pages/static/media/4.6c98b620.png"
+  },
+  {
+    "revision": "e1590eb6bf2dd3a9f44430bdddd0048e",
+    "url": "/Dasre/react-my-pages/static/media/DL.e1590eb6.png"
+  },
+  {
+    "revision": "d03b72755d95f70dcfecdc8287934300",
+    "url": "/Dasre/react-my-pages/static/media/mypicture.d03b7275.jpg"
+  }
+]);
